515cfa74e082420c74f6199a9d7b1ee88c3f1fa8
Muhammad Nabiel Andityo <mnabielap@gmail.com>
2023-05-06 21:02:43 +0700

    Add test case controller and service

--- src/test/java/id/ac/ui/cs/advprog/auth/service/auth/AuthenticationServiceTest.java
+++ src/test/java/id/ac/ui/cs/advprog/auth/service/auth/AuthenticationServiceTest.java
@@ -3,10 +3,15 @@ package id.ac.ui.cs.advprog.auth.service.auth;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.*;
 
+import id.ac.ui.cs.advprog.auth.dto.AuthenticationRequest;
+import id.ac.ui.cs.advprog.auth.dto.RegisterRequest;
+import id.ac.ui.cs.advprog.auth.exceptions.UserAlreadyExistException;
 import id.ac.ui.cs.advprog.auth.model.auth.User;
+import id.ac.ui.cs.advprog.auth.repository.TokenRepository;
 import id.ac.ui.cs.advprog.auth.repository.UserRepository;
 
 import id.ac.ui.cs.advprog.auth.service.AuthenticationService;
+import id.ac.ui.cs.advprog.auth.service.JwtService;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -16,7 +21,10 @@ import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.mockito.junit.jupiter.MockitoSettings;
 import org.mockito.quality.Strictness;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
+import java.util.NoSuchElementException;
 import java.util.Optional;
 
 @ExtendWith(MockitoExtension.class)
@@ -28,6 +36,15 @@ public class AuthenticationServiceTest {
 
     @Mock
     private UserRepository userRepository;
+
+    @Mock
+    private AuthenticationManager authenticationManager;
+    @Mock
+    private JwtService jwtService;
+    @Mock
+    TokenRepository tokenRepository;
+    @Mock
+    PasswordEncoder passwordEncoder;
 
     User user;
 
@@ -47,6 +64,24 @@ public class AuthenticationServiceTest {
     public void testWrongUsername() {
         Assertions.assertThrows(Exception.class, () -> {
             userRepository.findByUsername("wrongUsername").get();
+        });
+    }
+
+    @Test
+    public void testUserAlreadyExistException() {
+        when(userRepository.findByUsername(any(String.class))).thenReturn(Optional.of(new User()));
+
+        Assertions.assertThrows(UserAlreadyExistException.class, () -> {
+            service.register(RegisterRequest.builder().username("any").build());
+        });
+    }
+
+    @Test
+    public void testUserNotFoundWhenLogin() {
+        when(userRepository.findByUsername(any(String.class))).thenReturn(Optional.empty());
+
+        Assertions.assertThrows(NoSuchElementException.class, () -> {
+            service.authenticate(AuthenticationRequest.builder().username("any").build());
         });
     }
 }
